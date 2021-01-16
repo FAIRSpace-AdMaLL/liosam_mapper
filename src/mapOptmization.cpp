2eb26448f1c6bd16d84dd6e428d73981e94fd1c0
Marwan Taher <marokhaled99@gmail.com>
2021-01-16 14:09:35 +0000

    fixed global map visualisation inconsistency

--- src/mapOptmization.cpp
+++ src/mapOptmization.cpp
@@ -260,29 +260,29 @@ void mapOptimization::publishGlobalMap()
     pcl::PointCloud<PointType>::Ptr globalMapKeyFramesDS(new pcl::PointCloud<PointType>());
 
     // kd-tree to find near key frames to visualize
-    std::vector<int> pointSearchIndGlobalMap;
-    std::vector<float> pointSearchSqDisGlobalMap;
+    // std::vector<int> pointSearchIndGlobalMap;
+    // std::vector<float> pointSearchSqDisGlobalMap;
     // search near key frames to visualize
-    mtx.lock();
-    kdtreeGlobalMap->setInputCloud(cloudKeyPoses3D);
-    kdtreeGlobalMap->radiusSearch(cloudKeyPoses3D->back(), globalMapVisualizationSearchRadius, pointSearchIndGlobalMap, pointSearchSqDisGlobalMap, 0);
-    mtx.unlock();
+    // mtx.lock();
+    // kdtreeGlobalMap->setInputCloud(cloudKeyPoses3D);
+    // kdtreeGlobalMap->radiusSearch(cloudKeyPoses3D->back(), globalMapVisualizationSearchRadius, pointSearchIndGlobalMap, pointSearchSqDisGlobalMap, 0);
+    // mtx.unlock();
 
-    for (int i = 0; i < (int)pointSearchIndGlobalMap.size(); ++i)
-        globalMapKeyPoses->push_back(cloudKeyPoses3D->points[pointSearchIndGlobalMap[i]]);
-    // downsample near selected key frames
-    pcl::VoxelGrid<PointType> downSizeFilterGlobalMapKeyPoses; // for global map visualization
-    downSizeFilterGlobalMapKeyPoses.setLeafSize(globalMapVisualizationPoseDensity, globalMapVisualizationPoseDensity, globalMapVisualizationPoseDensity); // for global map visualization
-    downSizeFilterGlobalMapKeyPoses.setInputCloud(globalMapKeyPoses);
-    downSizeFilterGlobalMapKeyPoses.filter(*globalMapKeyPosesDS);
+    // for (int i = 0; i < (int)pointSearchIndGlobalMap.size(); ++i)
+    //     globalMapKeyPoses->push_back(cloudKeyPoses3D->points[pointSearchIndGlobalMap[i]]);
+    // // downsample near selected key frames
+    // pcl::VoxelGrid<PointType> downSizeFilterGlobalMapKeyPoses; // for global map visualization
+    // downSizeFilterGlobalMapKeyPoses.setLeafSize(globalMapVisualizationPoseDensity, globalMapVisualizationPoseDensity, globalMapVisualizationPoseDensity); // for global map visualization
+    // downSizeFilterGlobalMapKeyPoses.setInputCloud(globalMapKeyPoses);
+    // downSizeFilterGlobalMapKeyPoses.filter(*globalMapKeyPosesDS);
 
     // extract visualized and downsampled key frames
-    for (int i = 0; i < (int)globalMapKeyPosesDS->size(); ++i){
-        if (pointDistance(globalMapKeyPosesDS->points[i], cloudKeyPoses3D->back()) > globalMapVisualizationSearchRadius)
-            continue;
-        int thisKeyInd = (int)globalMapKeyPosesDS->points[i].intensity;
-        *globalMapKeyFrames += *transformPointCloud(cornerCloudKeyFrames[thisKeyInd],  &cloudKeyPoses6D->points[thisKeyInd]);
-        *globalMapKeyFrames += *transformPointCloud(surfCloudKeyFrames[thisKeyInd],    &cloudKeyPoses6D->points[thisKeyInd]);
+    for (int i = 0; i < (int)cloudKeyPoses6D->size(); ++i){
+        // if (pointDistance(globalMapKeyPosesDS->points[i], cloudKeyPoses3D->back()) > globalMapVisualizationSearchRadius)
+        //     continue;
+        // int thisKeyInd = (int)globalMapKeyPosesDS->points[i].intensity;
+        *globalMapKeyFrames += *transformPointCloud(cornerCloudKeyFrames[i],  &cloudKeyPoses6D->points[i]);
+        *globalMapKeyFrames += *transformPointCloud(surfCloudKeyFrames[i],    &cloudKeyPoses6D->points[i]);
     }
     // downsample visualized points
     pcl::VoxelGrid<PointType> downSizeFilterGlobalMapKeyFrames; // for global map visualization
@@ -1298,7 +1298,7 @@ void mapOptimization::addLoopFactor()
 
 void mapOptimization::saveKeyFramesAndFactor()
 {
-    if (saveFrame() == false)
+    if (saveFrame() == false || laserCloudCornerLastDS->size() == 0 || laserCloudSurfLastDS->size()== 0)
         return;
 
     // odom factor
