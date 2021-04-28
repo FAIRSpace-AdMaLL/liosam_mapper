4115f49a23b836f15c50b63156fc2c5bee247bb3
Marwan Taher <marokhaled99@gmail.com>
2021-04-28 19:35:32 +0100

    fixed imu lidar delay

--- src/imageProjection.cpp
+++ src/imageProjection.cpp
@@ -253,7 +253,7 @@ void ImageProjection::odomDeskewInfo()
 
     while (!odomQueue.empty())
     {
-        if (odomQueue.front().header.stamp.toSec() < timeScanCur - 0.01)
+        if (odomQueue.front().header.stamp.toSec() < timeScanCur - 0.1)
             odomQueue.pop_front();
         else
             break;
